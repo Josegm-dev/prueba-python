0fc24503dc30bc811808cab1a3c9e46dfd8c3828
Jose Garcia <324123845+Josegm-dev@users.noreply.github.com>
2026-09-17 17:05:01 +0200

    Añade cálculo de producción e ingresos

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-nombre = input("¿Como te llamas? ")
+numero_gallinas = int(input("¿Cuantas gallinas tienes?"))
+huevos_por_gallina= 200
+print("Tienes", numero_gallinas, "gallinas")
+huevos_totales = numero_gallinas * huevos_por_gallina
+print("Tus gallinas producen", huevos_totales, "huevos al año")
+docenas_totales = huevos_totales / 12
+print("Tu producción anual de huevos es de", docenas_totales, "docenas")
+docenas_completas = huevos_totales // 12
+print("Tu producción anual de huevos es de", docenas_completas, "docenas completas")
+huevos_sueltos = huevos_totales % 12
+print("Por lo tanto, te sobran", huevos_sueltos, "huevos")
+precio_docena = float(input("Cuál es el precio de venta por docena?"))
+ingresos_anuales = docenas_completas * precio_docena
+print(f"Ingresos anuales estimados por la venta de huevos: {ingresos_anuales:.2f} €")
+if numero_gallinas <= 30:
+    print("Autoconsumo")
+    print("No puedes vender huevos")
+elif numero_gallinas <=150:
+    print("Explotación reducida")
+    print("Puedes vender huevos")
+else:
+    print("Explotación ordinaria")
+    print("Puedes vender huevos")
 
-print("Hola", nombre)
-print("Este es mi primer proyecto con Git")
+print("Fin del programa")
